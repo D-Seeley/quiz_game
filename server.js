0fdc65cf017c89ebae95e23447e1404ca9cac47a
Daniel Seeley <Daniel.h.seeley@gmail.com>
2019-02-25 22:40:52 -0500

    second commit, adding file structure and view organization

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+
+const IoSingleton = require('./lib/server/IoSingleton');
+const { addIoListeners } = require('./lib/server/ioEvents');
+const { io } = new IoSingleton(server);
+// console.log(io);
+// const listeners = addSocketListeners(io);
 //Socket.io server event actions
-const socketListners = require('./lib/server/socketEvents').listeners(io);
+
 const path = require('path');
 const port = process.env.port || 1337;
 
 app.use(express.static(path.join(__dirname, './public')))
 
-io.on('connection', socketListners);
+io.on('connection', addIoListeners(io));
 server.listen(port, ()=> console.log(`server listening on port: ${port}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
+    entry: ['babel-polyfill', './/index.js'],
     output: {
       filename: 'main.js',
       path: __dirname + '/public'
