de185ad5edaf65ccdddbf88f19b4d1188866db31
Daniel Seeley <Daniel.h.seeley@gmail.com>
2019-03-19 18:39:41 -0400

    backup before starting over

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 
 const IoSingleton = require('./lib/server/IoSingleton');
-const { addIoListeners } = require('./lib/server/ioEvents');
+const { onConnection } = require('./lib/server/connectionHandler');
 const { io } = new IoSingleton(server);
 // console.log(io);
 // const listeners = addSocketListeners(io);
@@ -14,5 +14,5 @@ const port = process.env.port || 1337;
 
 app.use(express.static(path.join(__dirname, './public')))
 
-io.on('connection', addIoListeners(io));
+io.on('connection', onConnection(io));
 server.listen(port, ()=> console.log(`server listening on port: ${port}`));
